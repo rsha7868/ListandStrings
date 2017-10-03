f9bc205b98f427026cc80f5b717be747f953ac94
Sharp <rsha7868@740s-pl-127.local>
2017-10-03 13:48:44 -0600

    Fxing more data in list and array data files

--- src/list_strings/ListController.java
+++ src/list_strings/ListController.java
@@ -1,9 +1,35 @@
 package list_strings;
-
+import java.util.List;
+import java.util.ArrayList;
+import tools.model.Kahoot;
+import tools.view.Display;
 public class ListController
 {
+	private List<Kahoot> myKahoots;
+	private Display popup;
+	public ListController()
+	{
+		myKahoot = new ArrayList<Kahoot>();
+		popup = new Display();
+	}
+	
 	public void start()
 	{
-		
+		Kahoot myFirstKahoot = new Kahoot();
+		myKahoots.add(myFirstKahoot);
+		fillTheList();
+		showTheList();
+	}
+	private void fillTheList()
+	{
+		Kahoot TenWaysToDie = new Kahoot("Ryan", 10);
+		myKahoot.add(TenWaysToDie);
+	}
+	private void showTheList()
+	{
+		for (int index = 0; index < myKahoot.size();index += 1)
+		{
+			popup.displayText(myKahoots.get(index));
+		}
 	}
 }
